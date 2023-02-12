da5e189931a5cae6f980d086ff4ecd791a3d4684
maxbroxgames <124490874+maxbroxgames@users.noreply.github.com>
2023-02-12 10:15:12 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+function changecolor() {
+ let r = randomNumber(0,255)
+ let g = randomNumber(0,255)
+ let b = randomNumber(0,255)
+
+    document.body.style.background = `rgb(${r}, ${g}, ${b})`;
+}
+
+
+function randomNumber(min,max ) {
+    return Math.floor((Math.random() * (max - min)) + min)
+}
